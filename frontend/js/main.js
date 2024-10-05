f045b68bf86d9f4d4f3ad1e5c146bed129e86131
robiul26 <robiulrobi26@gmail.com>
2024-10-05 21:57:09 +0600

    responsive bug fixed

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -53,6 +53,36 @@
     });
 
 
+    // airport carousel
+    $(".airport-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: false,
+        dots: false,
+        loop: true,
+        margin: 25,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:2
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:2
+            },
+            1200:{
+                items:5
+            }
+        }
+    });
+
     // packages carousel
     $(".packages-carousel").owlCarousel({
         autoplay: true,
